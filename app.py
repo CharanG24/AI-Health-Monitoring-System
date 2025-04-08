82cb7b351abca1fb6b20179d6da3680dfeaeb321
Sri Charan Gurrala <128962279+CharanG24@users.noreply.github.com>
2025-04-07 20:19:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -208,3 +208,7 @@ def get_patient_analysis(patient_id):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True) 
+
+from vercel_wsgi import make_lambda_handler
+
+handler = make_lambda_handler(app)
